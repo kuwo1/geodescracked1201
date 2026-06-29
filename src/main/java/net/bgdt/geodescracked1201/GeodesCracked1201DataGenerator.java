a75dc8daadb99238e7b7a8e13bc9d4a79af5cca7
kuwo1 <rvkeely2.0@gmail.com>
2026-06-29 06:41:15 -0500

    Initial commit

---
+++ src/main/java/net/bgdt/geodescracked1201/GeodesCracked1201DataGenerator.java
@@ -0,0 +1,11 @@
+package net.bgdt.geodescracked1201;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GeodesCracked1201DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/bgdt/geodescracked1201/GeodesCrackedClient.java
@@ -0,0 +1,10 @@
+package net.bgdt.geodescracked1201;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeodesCrackedClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
